7f3d27ae30385014403263c9258b180b75e175b9
pvn <me@pedro-vicente.net>
2023-08-09 05:08:40 -0400

    define OPENSSL_ROOT_DIR

--- build.cmake.sh
+++ build.cmake.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-cmake -S . -B build --fresh
+
+mkdir -p build
 pushd build
-cmake --build . --parallel 9 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+cmake .. 
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+cmake .. 
+
+elif [[ "$OSTYPE" == "msys" ]]; then
+
+cmake .. --fresh -DOPENSSL_ROOT_DIR="F:/nostr_client_relay/ext/openssl-3.0.5"
+cmake --build .
+
+fi
+
+sleep 3
+cmake --build .
+
 popd
+exit
